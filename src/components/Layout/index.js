eef64c9204b8a7d789e360ac894f3156d6c4e5f3
Divyam <divyamojas17@gmail.com>
2023-03-15 10:05:47 +0530

    Fix Layout page tags

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,9 @@
-import './index.scss'
 import useWindowDimensions from '../../hooks/useWindowDimension'
 import Sidebar from '../Sidebar'
 import BottomBar from '../BottomBar'
 import { Outlet } from 'react-router-dom'
+
+import './index.scss'
 
 const Layout = () => {
   const { width } = useWindowDimensions()
@@ -13,6 +14,7 @@ const Layout = () => {
         <span className="tags top-tags">
           <span className="top-tag-doctype"> &lt;!doctype portfolio&gt;</span>
           <br />
+          <span className="top-tag-html"> &lt;html&gt;</span>
           <br />
           &lt;body&gt;
         </span>
@@ -23,8 +25,9 @@ const Layout = () => {
         <span className="tags bottom-tags">
           &lt;/body&gt;
           <br />
+          <span className="bottom-tag-html"> &lt;/html&gt;</span>
           <br />
-          <span className="bottom-tag-html"> Thanks for visiting my page!</span>
+          <span className="bottom-thanks"> Thanks for visiting my page!</span>
         </span>
       </div>
     </div>
