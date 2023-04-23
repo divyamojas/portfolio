0286c21b1f4159e56c71db04eceaf02fadaefb2e
Divyam Ojas <divyamojas17@gmail.com>
2023-04-23 22:02:43 +0530

    Initial commit

---
+++ src/assets/assets/objects/index.js
@@ -0,0 +1,25 @@
+import { mm, fundraiserr, todoozzz } from '../images/'
+
+const PROJECTS = [
+  {
+    title: 'Monday Morning',
+    snapshot: mm,
+    techstack: ['ReactJS', 'MaterialUI', 'MongoDB'],
+    link: 'https://mondaymorning.nitrkl.ac.in/',
+  },
+  {
+    title: 'Fundraiserrr',
+    snapshot: fundraiserr,
+    techstack: ['Solidity', 'ReactJS', 'TailwindCss', 'Thirdweb'],
+    link: 'https://fundraiserrr.netlify.app/',
+  },
+
+  {
+    title: 'Todoozzz',
+    snapshot: todoozzz,
+    techstack: ['ReactJS', 'MaterialUI', 'CSS'],
+    link: 'https://todoozzz.netlify.app/',
+  },
+]
+
+export { PROJECTS }
